32dce8a1021e19ebf3df2b637935c6c69c7c270c
Ronaldo Medrozo <rmedrozo@Ronaldos-MacBook-Pro.local>
2020-06-14 17:43:18 -0400

    Add example of posting to slack webhook

---
+++ 01-webotron/webotron/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup_cdn
+
+setup(
+    name='webotron-80',
+    version='0.1',
+    author='',
+    author_email='',
+    description="Webotron is a tool that deploy static website to AWS."
+    license='GPLv3+',
+    packages=['webotron'],
+    url='',
+    installs_requires=[
+        'click',
+        'boto3'
+    ],
+    entry_points='''
+        [console_scripts]
+        webotron=webotron.webotron:cli
+    '''
+)
